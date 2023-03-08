58d518d9dc69f5e902125e7e3889e489c9595bd1
spindlymist <ocrobin@gmail.com>
2023-03-07 16:02:25 -0800

    add tests for object::hash

--- src/object/hash.rs
+++ src/object/hash.rs
@@ -77,3 +77,68 @@ impl TryFrom<&[u8]> for ObjectHash {
         Ok(ObjectHash { raw })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bytes_to_string() {
+        let hash = ObjectHash::try_from([
+            0xfb, 0x8b, 0x51, 0x1f, 0x9a, 0x0b, 0xa8, 0xdd, 0x4a, 0xb9,
+            0x8d, 0x13, 0x3f, 0xdf, 0x23, 0x0b, 0xbb, 0x6b, 0xa5, 0xff,
+        ].as_slice()).unwrap();
+        assert_eq!(hash.to_string(), "fb8b511f9a0ba8dd4ab98d133fdf230bbb6ba5ff");
+    }
+
+    #[test]
+    fn rejects_short_bytes() {
+        let result = ObjectHash::try_from([0; 19].as_slice());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn rejects_long_bytes() {
+        let result = ObjectHash::try_from([0; 21].as_slice());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn string_to_bytes() {
+        let hash = ObjectHash::try_from("fb8b511f9a0ba8dd4ab98d133fdf230bbb6ba5ff").unwrap();
+        assert_eq!(hash.raw, [
+            0xfb, 0x8b, 0x51, 0x1f, 0x9a, 0x0b, 0xa8, 0xdd, 0x4a, 0xb9,
+            0x8d, 0x13, 0x3f, 0xdf, 0x23, 0x0b, 0xbb, 0x6b, 0xa5, 0xff,
+        ]);
+    }
+
+    #[test]
+    fn rejects_short_string() {
+        let result = ObjectHash::try_from(str::repeat("a", 39).as_str());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn rejects_long_string() {
+        let result = ObjectHash::try_from(str::repeat("a", 41).as_str());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn rejects_nonhex_string() {
+        let result = ObjectHash::try_from(str::repeat("g", 40).as_str());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn to_path() {
+        use std::path::Component;
+
+        let hash = ObjectHash::try_from("fb8b511f9a0ba8dd4ab98d133fdf230bbb6ba5ff").unwrap();
+        let path = hash.to_path();
+        let mut components = path.components();
+        assert_eq!(components.next(), Some(Component::Normal("fb".as_ref())));
+        assert_eq!(components.next(), Some(Component::Normal("8b511f9a0ba8dd4ab98d133fdf230bbb6ba5ff".as_ref())));
+        assert_eq!(components.next(), None);
+    }
+}
