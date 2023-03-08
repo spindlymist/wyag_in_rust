1664892f43c74f216fcec51f283d20abab4e2d6c
spindlymist <ocrobin@gmail.com>
2023-03-08 13:47:22 -0800

    add tests for init command

---
+++ tests/cmd_init.rs
@@ -0,0 +1,25 @@
+mod common;
+use common::*;
+
+use wyag::{
+    commands::{cmd_init, InitArgs},
+    repo::RepoError
+};
+
+#[test]
+fn init_fails_on_nonempty_directory() {
+    let (temp_dir, snapshot) = setup_snapshot("uninitialized", true);
+
+    {
+        let err = cmd_init(InitArgs {
+                path: Some("uninitialized".into()),
+            })
+            .unwrap_err()
+            .downcast::<RepoError>()
+            .unwrap();
+        assert!(matches!(err, RepoError::InitPathExists(_)));
+    }
+
+    // The init directory should be unchanged
+    assert_paths!(temp_dir.child("uninitialized"), snapshot);
+}
