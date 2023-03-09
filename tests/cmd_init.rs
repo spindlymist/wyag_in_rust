600fd0f4cc0dbc26347fb42226dbf65a0ef2b370
spindlymist <ocrobin@gmail.com>
2023-03-09 10:01:00 -0800

    add more tests for init command

--- tests/cmd_init.rs
+++ tests/cmd_init.rs
@@ -8,6 +8,22 @@ use wyag::{
 
 #[test]
 fn init_fails_on_nonempty_directory() {
+    let (temp_dir, snapshot) = setup_snapshot("uninitialized", false).unwrap();
+
+    let err = cmd_init(InitArgs {
+            path: None,
+        })
+        .unwrap_err()
+        .downcast::<RepoError>()
+        .unwrap();
+    assert!(matches!(err, RepoError::InitPathExists(_)));
+
+    // The init directory should be unchanged
+    assert_paths!(temp_dir, snapshot);
+}
+
+#[test]
+fn init_fails_on_nonempty_subdirectory() {
     let (temp_dir, snapshot) = setup_snapshot("uninitialized", true).unwrap();
 
     let err = cmd_init(InitArgs {
@@ -20,4 +36,22 @@ fn init_fails_on_nonempty_directory() {
 
     // The init directory should be unchanged
     assert_paths!(temp_dir.child("uninitialized"), snapshot);
+}
+
+#[test]
+fn init_succeeds_on_empty_directory() {
+    let (temp_dir, _) = setup_snapshot("empty", false).unwrap();
+
+    cmd_init(InitArgs { path: None }).unwrap();
+
+    assert_paths!(temp_dir, Snapshot::named("initialized").unwrap());
+}
+
+#[test]
+fn init_succeeds_on_empty_subdirectory() {
+    let (temp_dir, _) = setup_snapshot("empty", true).unwrap();
+
+    cmd_init(InitArgs { path: Some("empty".into()) }).unwrap();
+
+    assert_paths!(temp_dir.child("empty"), Snapshot::named("initialized").unwrap());
 }
